66ab0364b40af5a4a8399c499baeb83286f43ad4
Laurent BOURGEOIS <lbourgeois@talend.com>
2020-12-02 14:33:32 +0100

    Add GCS module group to HDI

---
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/hdi/HDIDistribution.java
@@ -0,0 +1,27 @@
+package org.talend.hadoop.distribution.hdi;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.AbstractDistribution;
+import org.talend.hadoop.distribution.ComponentType;
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.NodeComponentTypeBean;
+import org.talend.hadoop.distribution.constants.ModuleGroupName;
+import org.talend.hadoop.distribution.constants.SparkBatchConstant;
+import org.talend.hadoop.distribution.utils.ModuleGroupsUtils;
+
+public abstract class HDIDistribution extends AbstractDistribution{
+    
+    protected Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> buildNodeModuleGroups(
+            String distribution, String version) {
+        Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> result = new HashMap<>();
+        // GCS
+        result.put(new NodeComponentTypeBean(ComponentType.SPARKBATCH, SparkBatchConstant.GCS_CONFIG_COMPONENT), 
+                   ModuleGroupsUtils.getModuleGroups(distribution, version, (String) null, ModuleGroupName.GCS.get(this.getVersion()), true));
+        return result;
+    }
+    
+    
+}
